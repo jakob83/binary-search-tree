3c0d0f98f20e4630d2e41095ea700fa8c05c96fd
jakob83 <jakobpllo@gmail.com>
2024-09-10 17:20:31 +0200

    code efficiency improvements and less complex code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class Node {
-  constructor(data, leftTree, rightTree) {
+  constructor(data, leftTree = null, rightTree = null) {
     this.data = data;
     this.left = leftTree;
     this.right = rightTree;
@@ -9,20 +9,22 @@ class Node {
 class Tree {
   constructor(array) {
     this.array = this.cleanArray(array);
-    this.root = this.buildTree();
+    this.root = this.buildTree(this.array);
   }
 
   constructTree(array, start, end) {
-    if (start > end) return null;
+    if (!array || start > end) return null;
     const mid = Math.floor((start + end) / 2);
     const node = new Node(array[mid]);
     node.left = this.constructTree(array, start, mid - 1);
     node.right = this.constructTree(array, mid + 1, end);
-    this.root = node;
     return node;
   }
 
   cleanArray(array) {
+    if (!Array.isArray(array)) {
+      return null;
+    }
     let cleanArray = array;
     // sort array
     cleanArray.sort((a, b) => a - b);
@@ -35,12 +37,22 @@ class Tree {
     }
     return cleanArray;
   }
-  buildTree() {
-    this.root = this.constructTree(this.array, 0, this.array.length - 1);
+  buildTree(array) {
+    if (!Array.isArray(array)) {
+      return null;
+    }
+    this.root = this.constructTree(array, 0, array.length - 1);
     return this.root;
   }
 
   insert(value) {
+    if (!this.root) {
+      this.root = new Node(value, null, null);
+      return true;
+    }
+    if (typeof value !== 'number') {
+      return false;
+    }
     let curr = this.root;
     while (curr) {
       if (value === curr.data) {
@@ -61,71 +73,99 @@ class Tree {
     }
     return false;
   }
+
   remove(value) {
-    let curr = this.root;
-    while (curr) {
-      if (value < curr.data) {
-        // to remember the parent of the new initialized curr
-        let headNode = curr;
-        curr = curr.left;
-        // check if the new curr exists
-        if (curr) {
-          if (curr.data === value) {
-            // if curr is a leaf then just delete it
-            if (!curr.left && !curr.right) {
-              headNode.left = null;
-            }
-            // if curr has only one child set that child to be curr
-            else if ((curr.right && !curr.left) || (!curr.right && curr.left)) {
-              if (curr.right) {
-                headNode.left = curr.right;
-              } else {
-                headNode.left = curr.left;
-              }
-            }
-            // if curr has two children
-            else {
-              // search the next biggest element by going to the right subtree of curr and then always left, until there is no more
-              let curr2 = curr.right;
-              while (curr2.left) {
-                curr2 = curr2.left;
-              }
-              let copy = curr2.data;
-              this.remove(curr2.data);
-              curr.data = copy;
-            }
+    if (!this.root) throw new Error('no root');
+    let parent = this.root;
+    let child = parent.left;
+    if (value > parent.data) {
+      child = parent.right;
+    }
+    if (parent.data === value) {
+      child = parent;
+      parent = null;
+    }
+    while (child) {
+      if (child.data === value) {
+        // if child is a leaf then just delete it
+        if (!child.left && !child.right) {
+          if (!parent) {
+            this.root = null;
+            return true;
           }
+          if (parent.left === child) {
+            parent.left = null;
+          } else {
+            parent.right = null;
+          }
+          return true;
+        }
+        // if child has only one child set that child to be child of child => confusing i know
+        else if ((child.right && !child.left) || (!child.right && child.left)) {
+          if (!parent) {
+            if (child.right) {
+              this.root = child.right;
+            } else {
+              this.root = child.left;
+            }
+            return true;
+          }
+          if (child.right) {
+            parent.left = child.right;
+          } else {
+            parent.left = child.left;
+          }
+          return true;
+        }
+        // if child has two children
+        else {
+          // search the next biggest element by going to the right subtree of child and then always left, until there is no more
+          let child2 = child.right;
+          while (child2.left) {
+            child2 = child2.left;
+          }
+          let copy = child2.data;
+          this.remove(child2.data);
+          child.data = copy;
+          return true;
+        }
+      } else if (value > child.data) {
+        parent = child;
+        child = child.right;
+      } else {
+        parent = child;
+        child = child.left;
+      }
+    }
+  }
+
+  removeRec(value, child = this.root, parent = null) {
+    if (!child) return null;
+    if (value === child.data) {
+      if (!child.left || !child.right) {
+        const newChild = child.right ? child.right : child.left;
+        if (parent) {
+          let dir = 'left';
+          if (parent.right === child) dir = 'right';
+          parent[dir] = newChild;
+        } else {
+          this.root = newChild;
         }
       } else {
-        let headNode = curr;
-        curr = curr.right;
-        // check if the new curr exists
-        if (curr) {
-          if (curr.data === value) {
-            // if curr is a leaf then just delete it
-            if (!curr.left && !curr.right) {
-              headNode.right = null;
-            } else if (
-              (curr.right && !curr.left) ||
-              (!curr.right && curr.left)
-            ) {
-              if (curr.right) {
-                headNode.right = curr.right;
-              } else {
-                headNode.right = curr.left;
-              }
-            } else {
-              let curr2 = curr.right;
-              while (curr2.left) {
-                curr2 = curr2.left;
-              }
-              let copy = curr2.data;
-              this.remove(curr2.data);
-              curr.data = copy;
-            }
-          }
+        let sub = child.right;
+        let subPar = child;
+        while (sub.left) {
+          subPar = sub;
+          sub = sub.left;
         }
+        child.data = sub.data;
+        this.removeRec(sub.data, sub, subPar);
       }
+      return value;
+    } else if (value < child.data) {
+      return this.removeRec(value, child.left, child);
+    } else {
+      return this.removeRec(value, child.right, child);
     }
   }
 
@@ -145,10 +185,133 @@ class Tree {
       this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
     }
   }
+  find(value) {
+    if (typeof value !== 'number') return null;
+    let curr = this.root;
+    while (curr) {
+      if (curr.data === value) {
+        return curr;
+      } else if (curr.data > value) {
+        curr = curr.left;
+      } else {
+        curr = curr.right;
+      }
+    }
+    return null;
+  }
+  levelOrder(callback) {
+    if (!this.root) throw new Error('no root');
+    if (!callback) throw new Error('No callback function provided');
+    const queue = [this.root];
+    while (queue.length > 0) {
+      let curr = queue.shift();
+      if (curr.left) {
+        queue.push(curr.left);
+      }
+      if (curr.right) {
+        queue.push(curr.right);
+      }
+      callback(curr);
+    }
+  }
+  levelOrderRecursive(callback, queue = [this.root]) {
+    if (typeof callback != 'function')
+      throw new Error('No callback function provided');
+    if (queue.length <= 0) {
+      return;
+    }
+    let curr = queue.shift();
+    if (curr.left) {
+      queue.push(curr.left);
+    }
+    if (curr.right) {
+      queue.push(curr.right);
+    }
+    callback(curr);
+    this.levelOrderRecursive(callback, queue);
+  }
+
+  inOrder(node = this.root, callback) {
+    if (typeof callback !== 'function')
+      throw new Error('No callback function provided');
+    if (node === null) return;
+    this.inOrder(node.left, callback);
+    callback(node);
+    this.inOrder(node.right, callback);
+  }
+  preOrder(node = this.root, callback) {
+    if (typeof callback !== 'function')
+      throw new Error('No callback function provided');
+    if (node === null) {
+      return;
+    }
+    callback(node);
+    this.preOrder(node.left, callback);
+    this.preOrder(node.right, callback);
+  }
+  postOrder(callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('No callback function provided');
+    }
+    if (node === null) {
+      return;
+    }
+    this.preOrder(node.left, callback);
+    this.preOrder(node.right, callback);
+    callback(node);
+  }
+  height(node = this.root) {
+    let queue = [node];
+    let count = 0;
+    while (queue.length > 0) {
+      let newQueue = [];
+      queue.forEach((node) => {
+        if (node.left) {
+          newQueue.push(node.left);
+        }
+        if (node.right) {
+          newQueue.push(node.right);
+        }
+      });
+      queue = newQueue;
+      count += 1;
+    }
+    return count;
+  }
+  heightRec(node = this.root) {
+    if (!node) return 0;
+    return Math.max(
+      this.heightRec(node.right) + 1,
+      this.heightRec(node.left) + 1
+    );
+  }
+  depth(node) {
+    let curr = this.root;
+    let count = 1;
+    while (curr) {
+      if (node.data === curr.data) return count;
+      if (node.data > curr.data) {
+        if (!curr.right) return null;
+        curr = curr.right;
+      } else {
+        if (!curr.left) return null;
+        curr = curr.left;
+      }
+      count += 1;
+    }
+  }
+  isBalanced() {
+    if (!this.root) return true;
+    const leftHeight = this.height(this.root.left);
+    const rightHeight = this.height(this.root.right);
+    return leftHeight - rightHeight < 1 && leftHeight - rightHeight > -1;
+  }
+  rebalance() {
+    if (!this.root) {
+      return;
+    }
+    let array = [];
+    this.inOrder(this.root, (node) => array.push(node.data));
+    this.buildTree(array);
+  }
 }
-let tree = new Tree([
-  2, 1, 1, 3, 5, 6, 8, 2, 10, 11, 12, 9, 13, 14, 1, 4, 5, 1, 2, 6, 4, 3, 7, 5,
-]);
-tree.insert(0);
-tree.remove(8);
-console.log(tree.prettyPrint(tree.root));
